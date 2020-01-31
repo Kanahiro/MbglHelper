1a6f77a9fb6c8e00856bbbb345c7e9ded5f453ab
Kanahiro <mediterranean1769@gmail.com>
2020-01-31 23:10:48 +0900

    geojson module

--- src/geojson.js
+++ src/geojson.js
@@ -1,19 +1,66 @@
-let classify = function(feature) {
-    return feature.type
+let _classify = function(geojson) {
+    if (geojson.type === "FeatureCollection") {
+        return geojson.features[0].geometry.type
+    }
+    return geojson.geometry.type
+}
+
+let _randomColor = function() {
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+let _classifiedOptions = function(geometryType) {
+    switch (geometryType) {
+        case "MultiPolygon":
+        case "Polygon":
+            let polygonOptions = {
+                type:"fill",
+                paint:{
+                    'fill-color': _randomColor(),
+                    'fill-opacity': 0.8
+                }
+            }
+            return polygonOptions
+        case "MultiLineString":
+        case "String":
+            let lineOptions = {
+                type:"line",
+                paint:{
+                    'line-color': _randomColor(),
+                    'line-opacity': 0.8
+                }
+            }
+            return lineOptions
+        default:
+            break;
+    }
+}
+
+let _cleanOptions = function(options, geometryType) {
+    let defaultOptions = _classifiedOptions(geometryType)
+    if (options.type != undefined) {
+        defaultOptions.type = options.type
+    }
+    if (options.paint != undefined) {
+        defaultOptions.paint = options,paint
+    }
+    return defaultOptions
 }
 
 module.exports = {
-    import: function(map, geojson, id, type, paint) {
+    import: function(map, geojson, id, options={}) {
+        let geometryType = _classify(geojson)
+        let cleanedOptions = _cleanOptions(options, geometryType)
         map.addSource(id, {
             'type': 'geojson',
             'data': geojson
         });
         map.addLayer({
             'id': id,
-            'type': type,
+            'type': cleandOptions.type,
             'source': 'test',
             'layout': {},
-            'paint': paint
+            'paint': cleanedOptions.paint
         });
     }
 }
--- src/index.js
+++ src/index.js
@@ -7,70 +7,13 @@ class MbglWrapper {
 
     import(geojson, options={}) {
         let id = options.id
-        let type = options.type
-        let paint = options.paint
 
         if (id === undefined) {
             id = "test"
         }
-        
-        let geometryType = this._classify(geojson)
-        if (type === undefined) {
-            let options = this._classifiedOptions(geometryType)
-            type = options.type
-        }
-        if (paint === undefined) {
-            let options = this._classifiedOptions(geometryType)
-            paint = options.paint
-        }
 
-        gjLoader.import(this.map, geojson, id, type, paint)
+        gjLoader.import(this.map, geojson, id, options)
     }
-
-    _classify(geojson) {
-        if (geojson.type === "FeatureCollection") {
-            return geojson.features[0].geometry.type
-        }
-        return geojson.geometry.type
-    }
-
-    _classifiedOptions(geometryType) {
-        switch (geometryType) {
-            case "MultiPolygon":
-            case "Polygon":
-                return this._polygonOptions()
-            case "MultiLineString":
-            case "String":
-                return this._lineOptions()
-            default:
-                break;
-        }
-    }
-    
-    _polygonOptions() {
-        return {
-            type:"fill",
-            paint:{
-                'fill-color': this._randomColor(),
-                'fill-opacity': 0.8
-            }
-        }
-    }
-
-    _lineOptions() {
-        return {
-            type:"line",
-            paint:{
-                'line-color': this._randomColor(),
-                'line-opacity': 0.8
-            }
-        }
-    }
-
-    _randomColor() {
-        return "#" + Math.floor(Math.random() * 16777215).toString(16);
-    }
-
 }
 
 module.exports = MbglWrapper
